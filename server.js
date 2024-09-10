c18843e9855c9667f02fbb1a13d5d7364385d6f1
Márcia Agostinho <106019020+agostinhomarcia@users.noreply.github.com>
2024-09-10 20:52:20 -0300

    fix: changes deploy

--- server.js
+++ server.js
@@ -94,9 +94,5 @@ app.get('/users', async (req, res) => {
 });
 
 
-// const PORT = process.env.PORT || 3000;
-// app.listen(PORT, () => {
-//   console.log(`Servidor rodando na porta ${PORT}`);
-// });
 
 module.exports = app;
